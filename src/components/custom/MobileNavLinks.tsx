1aca21dcc567ee41461ade503d4124a8ee66da0e
devRezaulKarim <dev.rezaulkarim@gmail.com>
2024-05-15 00:20:48 -0600

    FEAT: Add navbar for large and small devices

---
+++ src/components/custom/MobileNavLinks.tsx
@@ -0,0 +1,47 @@
+import Link from "next/link";
+import { Dispatch, SetStateAction, useState } from "react";
+import { FaBars } from "react-icons/fa";
+import { FaXmark } from "react-icons/fa6";
+
+type prop = {
+  isOpenMobileNavLinks: boolean;
+  setIsOpenMobileNavLinks: Dispatch<SetStateAction<boolean>>;
+  setIsOpenMobileSearch: Dispatch<SetStateAction<boolean>>;
+};
+
+const MobileNavLinks = ({
+  isOpenMobileNavLinks,
+  setIsOpenMobileNavLinks,
+  setIsOpenMobileSearch,
+}: prop) => {
+  const navLinks: string[] = ["shop", "on sale", "new arrivals", "brands"];
+  const handleMobileMenuToggle = () => {
+    setIsOpenMobileNavLinks(!isOpenMobileNavLinks);
+    setIsOpenMobileSearch(false);
+  };
+  return (
+    <>
+      <button onClick={handleMobileMenuToggle} className="text-xl">
+        {isOpenMobileNavLinks ? <FaXmark /> : <FaBars />}
+      </button>
+      <ul
+        className={` bg-gray-200 absolute top-[100%] w-full text-center overflow-hidden divide-y-2 divide-gray-100 duration-200 ${
+          !isOpenMobileNavLinks ? "h-0" : "h-[152px] border-b"
+        }`}
+      >
+        {navLinks.map((link) => (
+          <li key={link} className="">
+            <Link
+              className="uppercase text-sm block w-full py-2"
+              href={`/${link}`}
+            >
+              {link}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+};
+
+export default MobileNavLinks;
--- src/components/custom/Navbar.tsx
+++ src/components/custom/Navbar.tsx
@@ -1,5 +1,98 @@
+"use client";
+import Link from "next/link";
+import { Input } from "../ui/input";
+import { IoSearch } from "react-icons/io5";
+import { MdOutlineShoppingCart } from "react-icons/md";
+import { IoPersonCircleOutline } from "react-icons/io5";
+
+import { useState } from "react";
+import MobileNavLinks from "./MobileNavLinks";
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const [isOpenMobileSearch, setIsOpenMobileSearch] = useState<boolean>(false);
+  const [isOpenMobileNavLinks, setIsOpenMobileNavLinks] =
+    useState<boolean>(false);
+
+  const navLinks: string[] = ["shop", "on sale", "new arrivals", "brands"];
+
+  const handleOpenMobileSearch = () => {
+    setIsOpenMobileNavLinks(false);
+    setIsOpenMobileSearch(!isOpenMobileSearch);
+  };
+
+  return (
+    <nav className="container mx-auto max-w-5x py-2 lg:py-4">
+      <div className="max-w-5xl mx-auto flex items-center gap-8 relative">
+        <div className="lg:hidden">
+          <MobileNavLinks
+            isOpenMobileNavLinks={isOpenMobileNavLinks}
+            setIsOpenMobileNavLinks={setIsOpenMobileNavLinks}
+            setIsOpenMobileSearch={setIsOpenMobileSearch}
+          />
+        </div>
+        <div>
+          <Link
+            href={"/"}
+            className="cursor-pointer text-3xl lg:text-4xl font-black"
+          >
+            eSHOP
+          </Link>
+        </div>
+        <ul className="lg:flex gap-4 hidden">
+          {navLinks.map((link) => (
+            <li key={link}>
+              <Link className="uppercase text-sm" href={`/${link}`}>
+                {link}
+              </Link>
+            </li>
+          ))}
+        </ul>
+        <div className="flex items-center gap-4 lg:flex-grow ml-auto lg:ml-0">
+          <form action="" className="relative flex-grow hidden lg:block">
+            <Input
+              type="text"
+              placeholder="Search for products..."
+              name="search"
+              className="rounded-full bg-[#f0f0f0] pl-8"
+            />
+            <IoSearch className="absolute left-5 top-1/2 -translate-y-1/2 -translate-x-1/2" />
+          </form>
+
+          {/* mobile devices search field trigger */}
+          <button
+            onClick={handleOpenMobileSearch}
+            className="lg:hidden text-xl"
+          >
+            <IoSearch />
+          </button>
+
+          <Link href={"/cart"} className="text-xl">
+            <MdOutlineShoppingCart />
+          </Link>
+          <Link href={"#"} className="text-xl">
+            <IoPersonCircleOutline />
+          </Link>
+        </div>
+      </div>
+
+      <form
+        action=""
+        className={`lg:hidden duration-200 overflow-hidden relative ${
+          isOpenMobileSearch ? "h-9" : "h-0"
+        }`}
+      >
+        <Input
+          type="text"
+          placeholder="Search for products..."
+          name="search"
+          className={`rounded-full bg-[#f0f0f0] shadow-none border-none pr-10`}
+        />
+        <button className="absolute right-7 top-1/2 -translate-y-1/2 translate-x-1/2 text-xl">
+          <IoSearch />
+        </button>
+      </form>
+    </nav>
+  );
 };
 
 export default Navbar;
